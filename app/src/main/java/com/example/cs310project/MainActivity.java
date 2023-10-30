46ca39a140256e5c3d9926d36763915224e23007
Gauri Shahra <shahra@usc.edu>
2023-10-29 19:10:32 -0700

    feature 1 changes

--- app/src/main/java/com/example/cs310project/MainActivity.java
+++ app/src/main/java/com/example/cs310project/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.csci310finalproject.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
