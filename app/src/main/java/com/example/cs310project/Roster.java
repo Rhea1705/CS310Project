1ac85b2e9a755933f80cce20e649e573bc0537f6
Gauri Shahra <shahra@usc.edu>
2023-11-04 17:14:26 -0700

    roster shows student. need to link chat

--- app/src/main/java/com/example/cs310project/Roster.java
+++ app/src/main/java/com/example/cs310project/Roster.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -31,44 +32,43 @@ public class Roster extends AppCompatActivity {
         Log.d("roster", "course: " + selectedCourse);
         Log.d("roster", "dept: " + department);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference rosterRef = database.getReference("departments").child(department).child("courses").child(selectedCourse);
+        DatabaseReference rosterRef = database.getReference("departments").child(department).child("courses").child(selectedCourse).child("roster");
         LinearLayout rosterList = findViewById(R.id.rosterList);
 
-//        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
-//        bottomNavigationView.setSelectedItemId(R.id.rosterList);
-//        Intent chatIntent = new Intent(this, ChatActivity.class);
-//        Intent classesIntent = new Intent(this, DepartmentsActivity.class);
-//        Intent profileIntent = new Intent(this, ProfileActivity.class);
-//
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//
-//                int itemId = item.getItemId();
-//                if (itemId == R.id.chat) {
-//                    startActivity(chatIntent);
-//                }
-//                else if (itemId == R.id.classes) {
-//                    startActivity(classesIntent);
-//                }
-//                else if (itemId == R.id.profile) {
-//                    startActivity(profileIntent);
-//                }
-//                return true;
-//            }
-//        });
-        rosterRef.child("roster").addListenerForSingleValueEvent(new ValueEventListener() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setSelectedItemId(R.id.rosterList);
+        Intent chatIntent = new Intent(this, ChatActivity.class);
+        Intent classesIntent = new Intent(this, DepartmentsActivity.class);
+        Intent profileIntent = new Intent(this, ProfileActivity.class);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.chat) {
+                    startActivity(chatIntent);
+                }
+                else if (itemId == R.id.classes) {
+                    startActivity(classesIntent);
+                }
+                else if (itemId == R.id.profile) {
+                    startActivity(profileIntent);
+                }
+                return true;
+            }
+        });
+        rosterRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 rosterList.removeAllViews();
                 for (DataSnapshot courseSnapshot : dataSnapshot.getChildren()) {
-                    
                     String name = courseSnapshot.child("name").getValue(String.class);
                     Log.d("roster", "student Name: " + name);
-                    LayoutInflater inflater = getLayoutInflater();
-                    View rosterView = inflater.inflate(R.layout.roster_item, rosterList, false);
-                    TextView studentNameView = rosterView.findViewById(R.id.name);
-                    studentNameView.setText(name);
+                    if (name != null) {
+                        createRosterItem(rosterList, name);
+                    }
+
 
                 }
             }
@@ -78,5 +78,24 @@ public class Roster extends AppCompatActivity {
                 Log.d("FirebaseError", error.getMessage());
             }
         });
+
+    }
+    private void createRosterItem(LinearLayout parentLayout, String studentName) {
+        LayoutInflater inflater = getLayoutInflater();
+        View rosterView = inflater.inflate(R.layout.roster_item, parentLayout, false);
+        TextView studentNameView = rosterView.findViewById(R.id.name);
+        studentNameView.setText(studentName);
+        Button chatBtn = rosterView.findViewById(R.id.chat);
+        chatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start the courses activity with the selected department
+                Intent intent = new Intent(Roster.this, ChatActivity.class);
+                intent.putExtra("selectedStudent", studentName);
+                startActivity(intent);
+            }
+        });
+        parentLayout.addView(rosterView);
+
     }
 }
