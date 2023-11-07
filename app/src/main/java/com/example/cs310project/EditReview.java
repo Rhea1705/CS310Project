0d268a03710b95dd0c1b2f60377eafa7558609b6
Gauri Shahra <shahra@usc.edu>
2023-11-06 22:50:20 -0800

    log out button

--- app/src/main/java/com/example/cs310project/EditReview.java
+++ app/src/main/java/com/example/cs310project/EditReview.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
