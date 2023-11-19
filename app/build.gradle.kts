7ec9c141e2513eb16e599308add25fcbcf019fc6
Rhea Mehta <mehtarhe@usc.edu>
2023-11-18 18:28:24 -0800

    gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
@@ -38,7 +39,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+//    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -53,8 +54,12 @@ dependencies {
     implementation("com.mikhaellopez:circularimageview:4.3.1")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+    testImplementation ("org.mockito:mockito-core:3.12.4")
 
+
+//    testImplementation ("org.mockito:mockito-core:3.12.4")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
